cacc9cd1bbc34ae7f9bc4fef94045f146bb7e4c1
PakornChaenglew <69179462+PakornChaenglew@users.noreply.github.com>
2020-10-27 11:39:32 +0700

    Update shape.py

--- pakorn-6230403882-lab9/geometry/shape.py
+++ pakorn-6230403882-lab9/geometry/shape.py
@@ -2,7 +2,12 @@ class Shape:
     def __init__(self, color):
         self.color = color
 
+    def __str__(self):
+        return f"Drawing a"
+
 class Circle(Shape):
+    n = 0
+    globals()['n'] = n
 
     def __init__(self, color, radius):
         self.radius = radius
@@ -14,7 +19,18 @@ class Circle(Shape):
     def radius(self, radius):
         return int(radius)
 
+    def draw(self):
+        global n
+        while True:
+            n += 1
+            return print(super().__str__() + f"circle with radius {self.radius}")
+
+    def get_num_circles():
+        return n
+
 class Rectangle(Shape):
+    e = 0
+    globals()['e'] = e
 
     def __init__(self, color, width, height):
         self.width = width
@@ -32,3 +48,16 @@ class Rectangle(Shape):
 
     def height(self, height):
         return int(height)
+
+    def draw(self):
+        return print(super().__str__() + f"rectangle with width {self.width} height {self.height}")
+
+    def print_area(self):
+        global e
+        while True:
+            e += 1
+            area = self.width * self.height
+            return print(f"Rectangle width {self.width} height {self.height} has area as {area}")
+
+    def get_num_rectangles():
+        return e
